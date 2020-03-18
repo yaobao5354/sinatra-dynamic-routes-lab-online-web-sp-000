9adafeea1a295319664bc9a9a005a8f1c270652d
yaoyu bao <yaobao5354@gmail.com>
2020-03-18 17:36:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-  get '/reverse/:name' do
+  get '/reversename/:name' do
     param[:name].reverse
   end
 end
