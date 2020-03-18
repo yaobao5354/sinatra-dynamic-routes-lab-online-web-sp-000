65d80d82643a748422c5c100d31a1d0a77ce2138
yaoyu bao <yaobao5354@gmail.com>
2020-03-18 18:58:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,43 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
 
+  get '/square/:number' do
+    @num = params[:number].to_i
+    "#{@num * @num}"
+  end
+
+  get '/say/:number/:phrase' do
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    @phrase * @num
+    #erb :saynumberphrase
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] + "."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    case @operation
+    when "add"
+      @oper = "+"
+    when "subtract"
+      @oper = "-"
+    when "multiply"
+      @oper = "*"
+    when "divide"
+      @oper = "/"
+    end
+
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    @result = @num1.send "#{@oper}", @num2
+    "#{@result}"
+  end
 end
